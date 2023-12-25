9ebd6cebdf69cd9606c8ae209cfb82ed9ab85cd8
Anton Chekulaev <metaphore@bk.ru>
2023-12-25 23:24:38 +0100

    Cleanup.

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,9 @@ const c = @cImport({
 const math = @import("std").math;
 const log = @import("std").log;
 const assert = @import("std").debug.assert;
-const sod = @import("sod.zig");
+
 const zlm = @import("zlm/zlm.zig");
+const sod = @import("sod.zig");
 
 const WINDOW_WIDTH: i32 = 800;
 const WINDOW_HEIGHT: i32 = 640;
@@ -18,6 +19,24 @@ const BAR_SIZE = zlm.vec2(128.0, 16.0);
 const BAR_HSIZE = BAR_SIZE.scale(0.5);
 const BAR_Y: f32 = @as(f32, WINDOW_HEIGHT) - 128;
 const BAR_SPEED: f32 = 720.0;
+
+var proj_pos = zlm.Vec2{
+    .x = WINDOW_WIDTH / 2,
+    .y = WINDOW_HEIGHT / 2,
+};
+var proj_dir = zlm.vec2(1, 0).rotate(30.0);
+
+var bar_x: f32 = @floatFromInt(WINDOW_WIDTH / 2);
+var bar_pos_sod = sod.Sod2.new(2.0, 0.6, 0.0);
+
+const InputState = struct {
+    left: bool,
+    right: bool,
+};
+var input = InputState {
+    .left = false,
+    .right = false,
+};
 
 pub fn main() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
@@ -38,22 +57,7 @@ pub fn main() !void {
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    var proj_pos = zlm.Vec2{
-        .x = WINDOW_WIDTH / 2,
-        .y = WINDOW_HEIGHT / 2,
-    };
-    var proj_dir = zlm.Vec2{
-        .x = 1.0,
-        .y = 0.0,
-    };
-    proj_dir = proj_dir.rotate(30.0);
-
-    var bar_x: f32 = @floatFromInt(WINDOW_WIDTH / 2);
-    var bar_pos_sod = sod.Sod2D(f32).init(2.0, 0.6, 0.0);
     bar_pos_sod.reset(bar_x, BAR_Y);
-
-    var input_left = false;
-    var input_right = false;
 
     var quit = false;
     while (!quit) {
@@ -66,10 +70,10 @@ pub fn main() !void {
                 c.SDL_KEYDOWN => {
                     switch (event.key.keysym.sym) {
                         c.SDLK_a => {
-                            input_left = true;
+                            input.left = true;
                         },
                         c.SDLK_d => {
-                            input_right = true;
+                            input.right = true;
                         },
                         else => {},
                     }
@@ -77,10 +81,10 @@ pub fn main() !void {
                 c.SDL_KEYUP => {
                     switch (event.key.keysym.sym) {
                         c.SDLK_a => {
-                            input_left = false;
+                            input.left = false;
                         },
                         c.SDLK_d => {
-                            input_right = false;
+                            input.right = false;
                         },
                         else => {},
                     }
@@ -89,69 +93,82 @@ pub fn main() !void {
             }
         }
 
-        var bar_dir_x: f32 = 0;
-        if (input_left) {
-            bar_dir_x += -1;
-        }
-        if (input_right) {
-            bar_dir_x += 1;
-        }
-        bar_x += bar_dir_x * BAR_SPEED * DELTA_TIME;
-        bar_x = math.clamp(bar_x, BAR_HSIZE.x, @as(f32, WINDOW_WIDTH) - BAR_HSIZE.x);
-        bar_pos_sod.update(DELTA_TIME, bar_x, BAR_Y);
+        update(DELTA_TIME);
 
-        if (proj_pos.x - PROJ_HSIZE <= 0 or (proj_pos.x + PROJ_HSIZE >= WINDOW_WIDTH)) {
-            proj_dir.x = -proj_dir.x;
-        }
-        if (proj_pos.y - PROJ_HSIZE <= 0 or (proj_pos.y + PROJ_HSIZE >= WINDOW_HEIGHT)) {
-            proj_dir.y = -proj_dir.y;
-        }
-
-        // Check platform hit.
-        const proj_dst_to_bar_h: f32 = bar_pos_sod.pos_y - (proj_pos.y + PROJ_HSIZE);
-        if (proj_dir.y > 0 and
-            (proj_dst_to_bar_h > -BAR_HSIZE.y and proj_dst_to_bar_h < BAR_HSIZE.y) and
-            (proj_pos.x + PROJ_HSIZE > bar_pos_sod.pos_x - BAR_HSIZE.x and proj_pos.x - PROJ_HSIZE < bar_pos_sod.pos_x + BAR_HSIZE.x))
-        {
-            // Add a little hit feedback to the platform.
-            const feedback: f32 = 640;
-            bar_pos_sod.acc_x += feedback * proj_dir.x;
-            bar_pos_sod.acc_y += feedback * proj_dir.y;
-
-            proj_dir.y = -proj_dir.y;
-        }
-
-        proj_pos.x += DELTA_TIME * PROJ_SPEED * proj_dir.x;
-        proj_pos.y += DELTA_TIME * PROJ_SPEED * proj_dir.y;
-        // log.debug("proj_pos is [{d}, {d}]", .{ proj_pos.x, proj_pos.y });
-        // log.debug("proj_dir is [{d}, {d}]", .{ proj_dir.x, proj_dir.y });
-
-        // RENDER ===========================================================================
-        _ = c.SDL_SetRenderDrawColor(renderer, 0x20, 0x20, 0x40, 0xff);
-        _ = c.SDL_RenderClear(renderer);
-
-        const proj_rect = c.SDL_Rect{
-            .x = @intFromFloat(proj_pos.x - PROJ_HSIZE),
-            .y = @intFromFloat(proj_pos.y - PROJ_HSIZE),
-            .w = @intFromFloat(PROJ_SIZE),
-            .h = @intFromFloat(PROJ_SIZE),
-        };
-        _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0x20, 0x40, 0xff);
-        _ = c.SDL_RenderFillRect(renderer, &proj_rect);
-
-        const bar_rect = c.SDL_Rect{
-            .x = roundInt(bar_pos_sod.pos_x - BAR_HSIZE.x),
-            .y = roundInt(bar_pos_sod.pos_y - BAR_HSIZE.y),
-            .w = BAR_SIZE.x,
-            .h = BAR_SIZE.y,
-        };
-        _ = c.SDL_SetRenderDrawColor(renderer, 0x20, 0xff, 0x20, 0xff);
-        _ = c.SDL_RenderFillRect(renderer, &bar_rect);
-
-        c.SDL_RenderPresent(renderer);
-
-        c.SDL_Delay(DELTA_TIME_INT);
+        render(renderer);
     }
+}
+
+fn update(delta_time: f32) void {
+    var bar_dir_x: f32 = 0;
+    if (input.left) {
+        bar_dir_x += -1;
+    }
+    if (input.right) {
+        bar_dir_x += 1;
+    }
+    bar_x += bar_dir_x * BAR_SPEED * delta_time;
+    bar_x = math.clamp(bar_x, BAR_HSIZE.x, @as(f32, WINDOW_WIDTH) - BAR_HSIZE.x);
+    bar_pos_sod.update(delta_time, bar_x, BAR_Y);
+
+    if (proj_pos.x - PROJ_HSIZE <= 0 or (proj_pos.x + PROJ_HSIZE >= WINDOW_WIDTH)) {
+        proj_dir.x = -proj_dir.x;
+    }
+    if (proj_pos.y - PROJ_HSIZE <= 0 or (proj_pos.y + PROJ_HSIZE >= WINDOW_HEIGHT)) {
+        proj_dir.y = -proj_dir.y;
+    }
+
+    // Check platform hit.
+    const proj_dst_to_bar_h: f32 = bar_pos_sod.pos_y - (proj_pos.y + PROJ_HSIZE);
+    if (proj_dir.y > 0 and
+        (proj_dst_to_bar_h > -BAR_HSIZE.y and proj_dst_to_bar_h < BAR_HSIZE.y) and
+        (proj_pos.x + PROJ_HSIZE > bar_pos_sod.pos_x - BAR_HSIZE.x and proj_pos.x - PROJ_HSIZE < bar_pos_sod.pos_x + BAR_HSIZE.x))
+    {
+        // Add a little hit feedback to the platform.
+        const feedback: f32 = 640;
+        bar_pos_sod.acc_x += feedback * proj_dir.x;
+        bar_pos_sod.acc_y += feedback * proj_dir.y;
+
+        proj_dir.y = -proj_dir.y;
+    }
+
+    proj_pos.x += delta_time * PROJ_SPEED * proj_dir.x;
+    proj_pos.y += delta_time * PROJ_SPEED * proj_dir.y;
+}
+
+fn render(renderer: *c.SDL_Renderer) void {
+    _ = c.SDL_SetRenderDrawColor(renderer, 0x20, 0x20, 0x40, 0xff);
+    _ = c.SDL_RenderClear(renderer);
+
+    const proj_rect = projRect();
+    _ = c.SDL_SetRenderDrawColor(renderer, 0xff, 0x20, 0x40, 0xff);
+    _ = c.SDL_RenderFillRect(renderer, &proj_rect);
+
+    const bar_rect = barRect();
+    _ = c.SDL_SetRenderDrawColor(renderer, 0x20, 0xff, 0x20, 0xff);
+    _ = c.SDL_RenderFillRect(renderer, &bar_rect);
+
+    c.SDL_RenderPresent(renderer);
+
+    c.SDL_Delay(DELTA_TIME_INT);
+}
+
+fn projRect() c.SDL_Rect {
+    return c.SDL_Rect{
+        .x = @intFromFloat(proj_pos.x - PROJ_HSIZE),
+        .y = @intFromFloat(proj_pos.y - PROJ_HSIZE),
+        .w = @intFromFloat(PROJ_SIZE),
+        .h = @intFromFloat(PROJ_SIZE),
+    };
+}
+
+fn barRect() c.SDL_Rect {
+    return c.SDL_Rect{
+        .x = roundInt(bar_pos_sod.pos_x - BAR_HSIZE.x),
+        .y = roundInt(bar_pos_sod.pos_y - BAR_HSIZE.y),
+        .w = BAR_SIZE.x,
+        .h = BAR_SIZE.y,
+    };
 }
 
 fn roundInt(value: f32) i32 {
--- src/sod.zig
+++ src/sod.zig
@@ -1,75 +1,161 @@
 const math = @import("std").math;
 
-pub fn Sod2D(comptime T: type) type {
+pub fn SpecializeOn(comptime T: type) type {
     return struct {
-        const Self = @This();
+        
+        pub const Sod2 = struct {
+            const Self = @This();
 
-        k1: T,
-        k2: T,
-        k3: T,
-        p_dst_x: T,
-        p_dst_y: T,
-        pos_x: T,
-        pos_y: T,
-        acc_x: T,
-        acc_y: T,
+            k1: T,
+            k2: T,
+            k3: T,
+            p_dst_x: T,
+            p_dst_y: T,
+            pos_x: T,
+            pos_y: T,
+            acc_x: T,
+            acc_y: T,
 
-        pub fn init(f: T, z: T, r: T) Sod2D(T) {
-            var self = Sod2D(T){
-                .k1 = undefined,
-                .k2 = undefined,
-                .k3 = undefined,
+            pub fn new(f: T, z: T, r: T) Self {
+                var self = Self{
+                    .k1 = undefined,
+                    .k2 = undefined,
+                    .k3 = undefined,
 
-                .p_dst_x = 0,
-                .p_dst_y = 0,
+                    .p_dst_x = 0,
+                    .p_dst_y = 0,
 
-                .pos_x = 0,
-                .pos_y = 0,
+                    .pos_x = 0,
+                    .pos_y = 0,
 
-                .acc_x = 0,
-                .acc_y = 0,
-            };
-            self.configure(f, z, r);
-            return self;
-        }
-
-        pub fn configure(self: *Self, f: T, z: T, r: T) void {
-            const pi = math.pi;
-            self.k1 = z / (pi * f);
-            self.k2 = 1 / ((2 * pi * f) * (2 * pi * f));
-            self.k3 = r * z / (2 * pi * f);
-        }
-
-        pub fn reset(self: *Self, dst_x: T, dst_y: T) void {
-            self.p_dst_x = dst_x;
-            self.p_dst_y = dst_y;
-            self.pos_x = dst_x;
-            self.pos_y = dst_y;
-            self.acc_x = 0;
-            self.acc_y = 0;
-        }
-
-        pub fn update(self: *Self, delta_time: T, dst_x: T, dst_y: T) void {
-            if (delta_time == 0) {
-                return;
+                    .acc_x = 0,
+                    .acc_y = 0,
+                };
+                self.configure(f, z, r);
+                return self;
             }
 
-            // Estimate velocity.
-            const xd_x: T = (dst_x - self.p_dst_x) / delta_time;
-            const xd_y: T = (dst_y - self.p_dst_y) / delta_time;
-            self.p_dst_x = dst_x;
-            self.p_dst_y = dst_y;
+            pub fn configure(self: *Self, f: T, z: T, r: T) void {
+                const pi = math.pi;
+                self.k1 = z / (pi * f);
+                self.k2 = 1 / ((2 * pi * f) * (2 * pi * f));
+                self.k3 = r * z / (2 * pi * f);
+            }
 
-            // Clamp k2 to guarantee stability without jitter.
-            const k2_stable: T = @max(self.k2, @max(delta_time * delta_time / 2 + delta_time * self.k1 / 2, delta_time * self.k1));
+            pub fn reset(self: *Self, dst_x: T, dst_y: T) void {
+                self.p_dst_x = dst_x;
+                self.p_dst_y = dst_y;
+                self.pos_x = dst_x;
+                self.pos_y = dst_y;
+                self.acc_x = 0;
+                self.acc_y = 0;
+            }
 
-            // Integrate position by velocity.
-            self.pos_x += delta_time * self.acc_x;
-            self.pos_y += delta_time * self.acc_y;
+            pub fn update(self: *Self, delta_time: T, dst_x: T, dst_y: T) void {
+                if (delta_time == 0) {
+                    return;
+                }
 
-            // Integrate velocity by acceleration.
-            self.acc_x += delta_time * (dst_x + self.k3 * xd_x - self.pos_x - self.k1 * self.acc_x) / k2_stable;
-            self.acc_y += delta_time * (dst_y + self.k3 * xd_y - self.pos_y - self.k1 * self.acc_y) / k2_stable;
-        }
+                // Estimate velocity.
+                const xd_x: T = (dst_x - self.p_dst_x) / delta_time;
+                const xd_y: T = (dst_y - self.p_dst_y) / delta_time;
+                self.p_dst_x = dst_x;
+                self.p_dst_y = dst_y;
+
+                // Clamp k2 to guarantee stability without jitter.
+                const k2_stable: T = @max(self.k2, @max(delta_time * delta_time / 2 + delta_time * self.k1 / 2, delta_time * self.k1));
+
+                // Integrate position by velocity.
+                self.pos_x += delta_time * self.acc_x;
+                self.pos_y += delta_time * self.acc_y;
+
+                // Integrate velocity by acceleration.
+                self.acc_x += delta_time * (dst_x + self.k3 * xd_x - self.pos_x - self.k1 * self.acc_x) / k2_stable;
+                self.acc_y += delta_time * (dst_y + self.k3 * xd_y - self.pos_y - self.k1 * self.acc_y) / k2_stable;
+            }
+        };
+
+        pub const sod2 = Sod2.new;
+
+        
+        // pub const Sod1 = SodBase(1);
+        // pub const Sod2 = SodBase(2);
+        // pub const Sod3 = SodBase(3);
+        // pub const Sod4 = SodBase(4);
+
+        // pub const sod1 = Sod1.new;
+        // pub const sod2 = Sod2.new;
+        // pub const sod3 = Sod3.new;
+        // pub const sod4 = Sod4.new;
+       
+        // fn SodBase(comptime size: usize) type { 
+        //     return struct {
+        //         const Self = @This();
+
+        //         const Dimen = struct {
+        //             p_dst: T,
+        //             pos: T,
+        //             acc: T,
+        //         };
+                
+        //         k1: T,
+        //         k2: T,
+        //         k3: T,
+        //         dimens: [size]Dimen,
+
+        //         pub fn new(f: T, z: T, r: T, dsts: [size]T) Self {
+        //             var self = Self{
+        //                 .k1 = undefined,
+        //                 .k2 = undefined,
+        //                 .k3 = undefined,
+        //                 .dimens = undefined,
+        //             };
+        //             self.configure(f, z, r);
+        //             self.reset(dsts);
+        //             return self;
+        //         }
+
+        //         pub fn configure(self: *Self, f: T, z: T, r: T) void {
+        //             const pi = math.pi;
+        //             self.k1 = z / (pi * f);
+        //             self.k2 = 1 / ((2 * pi * f) * (2 * pi * f));
+        //             self.k3 = r * z / (2 * pi * f);
+        //         }
+
+        //         pub fn reset(self: *Self, dsts: [size]T) void {
+        //             for (0.., dsts) |i, dst| {
+        //                 const dimen = &self.dimens[i];
+        //                 dimen.p_dst = dst;
+        //                 dimen.pos = dst;
+        //                 dimen.acc = 0;
+        //             }
+        //         }
+
+        //         pub fn pos(self: *Self, index: usize) T {
+        //             return self.dimens[index].pos;
+        //         }
+
+        //         pub fn update(self: *Self, delta_time: T, dsts: [size]T) void {
+        //             if (delta_time == 0) {
+        //                 return;
+        //             }
+
+        //             // Clamp k2 to guarantee stability without jitter.
+        //             const k2_stable: T = @max(self.k2, @max(delta_time * delta_time / 2 + delta_time * self.k1 / 2, delta_time * self.k1));
+
+        //             for (0.., &self.dimens) |i, *dimen| {
+        //                 const dst = dsts[i];
+        //                 // Estimate velocity.
+        //                 const xd: T = (dst - dimen.p_dst) / delta_time;
+        //                 // Integrate position by velocity.
+        //                 dimen.pos += delta_time * dimen.acc;
+        //                 // Integrate velocity by acceleration.
+        //                 dimen.acc += delta_time * (dst + self.k3 * xd - dimen.pos - self.k1 * dimen.acc) / k2_stable;
+        //             }
+        //         }
+        //     };
+        // }
     };
 }
+
+pub usingnamespace SpecializeOn(f32);
